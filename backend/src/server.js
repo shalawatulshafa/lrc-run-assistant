b7ba8e704133d59af77b56828e27b1fc9cd0944d
Kayla Pramudio Bagas Aryasatya <kayla.pramudio@gmail.com>
2026-05-06 15:01:13 +0700

    update port dan script untuk deploy dengan railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
